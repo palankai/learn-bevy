bd99ef2c4e582b3eb82ba1d56c5b6aed60d3a392
Csaba Palankai <csaba.palankai@gmail.com>
2023-04-15 22:58:51 +0100

    ep08 code - system sets

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -8,13 +8,20 @@ use systems::*;
 pub const PLAYER_SIZE: f32 = 64.0;
 pub const PLAYER_SPEED: f32 = 500.0;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+        app.configure_set(MovementSystemSet.before(ConfinementSystemSet))
+            .add_startup_system(spawn_player)
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }

9094a80c4ef3461321275c21501b02ad1c77d900
Csaba Palankai <csaba.palankai@gmail.com>
2023-04-15 14:02:59 +0100

    ep02 implementation

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Learn Bevy
 
-This repository follows the [learn-bevy](https://github.com/frederickjjoubert/learn-bevy) tutorial.
+This repository follows the following tutorials:
+- [learn-bevy](https://github.com/frederickjjoubert/learn-bevy)
+- [bevy-ball-game](https://github.com/frederickjjoubert/bevy-ball-game)
 
 [Youtube playlist of Learn Bevy 0.10](https://www.youtube.com/watch?v=TQt-v_bFdao&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd)
+
+
+# Notes
+
+## Gaming assets
+Great asset library: https://kenney.nl/
--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,38 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(PeoplePlugin)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_palyer)
         .run();
 }
 
-pub struct PeoplePlugin;
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
 
-impl Plugin for PeoplePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system(setup)
-            .add_system(print_names)
-            .add_system(people_with_jobs)
-            .add_system(person_does_job);
-    }
-}
-
-pub fn setup(mut commands: Commands) {
-    commands.spawn((
-        Person {
-            name: "Alex".to_string(),
-        },
-        Employed { job: Job::Doctor },
-    ));
-    commands.spawn(Person {
-        name: "Bob".to_string(),
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
     });
-    commands.spawn((
-        Person {
-            name: "Charlie".to_string(),
-        },
-        Employed {
-            job: Job::FireFighter,
-        },
-    ));
-    commands.spawn((
-        Person {
-            name: "David".to_string(),
-        },
-        Employed { job: Job::Lawyer },
-    ));
-    commands.spawn((
-        Person {
-            name: "Ellen".to_string(),
-        },
-        Employed {
-            job: Job::FireFighter,
-        },
-    ));
-}
-
-pub fn print_names(person_query: Query<&Person>) {
-    for person in person_query.iter() {
-        println!("Name: {}", person.name);
-    }
-}
-
-pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
-    for person in person_query.iter() {
-        println!("{} has a job", person.name);
-    }
-}
-
-pub fn people_ready_for_hire(person_query: Query<&Person, Without<Employed>>) {
-    for person in person_query.iter() {
-        println!("{} is ready for hire", person.name);
-    }
-}
-
-pub fn person_does_job(person_query: Query<(&Person, &Employed)>) {
-    for (person, employed) in person_query.iter() {
-        let job_name = match employed.job {
-            Job::Doctor => "Doctor",
-            Job::FireFighter => "Fire Fighter",
-            Job::Lawyer => "Lawyer",
-        };
-        println!("{} is a {}", person.name, job_name);
-    }
 }
 
 #[derive(Component)]
-pub struct Person {
-    pub name: String,
-}
+pub struct Player {}
 
-#[derive(Component)]
-pub struct Employed {
-    pub job: Job,
-}
-
-#[derive(Debug)]
-pub enum Job {
-    Doctor,
-    FireFighter,
-    Lawyer,
+pub fn spawn_palyer(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
 }
